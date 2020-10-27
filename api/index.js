fe015b93fe709472c4ee0ccfdcf0cd85e16e6b2e
Ally Murray <allymurray88@gmail.com>
2020-10-27 19:26:19 +0000

    Socket IO successfully emitting events

--- api/index.js
+++ api/index.js
@@ -1,8 +1,17 @@
-const http = require('http');
 const app = require('./app'); // the actual Express app
+const http = require('http');
+const socketIo = require('socket.io');
 const config = require('./utils/config');
 
 const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+  const interval = setInterval(() => socket.emit('hello-world', `Hello World ${new Date()}`), 1000);
+  socket.on('disconnect', () => {
+    clearInterval(interval);
+  });
+});
 
 server.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`);
