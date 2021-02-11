b3b7246d72c0e1b8891c1154a4f681393f438c95
Ally Murray <allymurray88@gmail.com>
2021-02-11 15:01:37 +0000

    Updated unit test description

--- api/src/components/awaken/awaken.test.js
+++ api/src/components/awaken/awaken.test.js
@@ -1,7 +1,7 @@
 const { awaken } = require('./awaken');
 
 describe('awaken', () => {
-  test('should return 200', async () => {
+  test('should call res.sendStatus with 200', async () => {
     const req = {};
     const res = { sendStatus: jest.fn() };
     await awaken(req, res);
